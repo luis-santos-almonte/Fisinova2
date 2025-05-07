92e63568f1c20f68b58f26f52977485dd3e3a0f6
FrankLDF <frankluisduarte00@gmail.com>
2025-05-06 21:27:16 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Logo from '@assets/Logo.png'
+
+function App() {
+  return (
+    <>
+      <img src={Logo} style={{ width: '50%' }} alt="logo de fisinova" />
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [react()],
+    server: {
+      port: env.VITE_PORT ? parseInt(env.VITE_PORT) : 5173,
+    },
+    resolve: {
+      alias: {
+        '@assets': path.resolve(__dirname, './src/assets'),
+        '@store': path.resolve(__dirname, './src/store'),
+        '@services': path.resolve(__dirname, './src/services'),
+        '@pages': path.resolve(__dirname, './src/pages'),
+        '@components': path.resolve(__dirname, './src/components'),
+        '@hooks': path.resolve(__dirname, './src/hooks'),
+        '@adapters': path.resolve(__dirname, './src/adapters'),
+        '@interceptors': path.resolve(__dirname, './src/interceptors'),
+        '@models': path.resolve(__dirname, './src/models'),
+      },
+    },
+  }
+})
