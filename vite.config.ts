5f10c4c5a60061789efbac78063a195ce097ec8c
FrankLDF <frankluisduarte00@gmail.com>
2025-05-08 21:49:48 -0400

    Adding initial shadcn/ui configuration for styles

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     server: {
       port: env.VITE_PORT ? parseInt(env.VITE_PORT) : 5173,
     },
@@ -17,6 +18,7 @@ export default defineConfig(({ mode }) => {
     },
     resolve: {
       alias: {
+        '@': path.resolve(__dirname, './src'),
         '@assets': path.resolve(__dirname, './src/assets'),
         '@store': path.resolve(__dirname, './src/store'),
         '@services': path.resolve(__dirname, './src/services'),
