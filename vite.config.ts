b75e3697ac60b5b97b3b2a095f95fa3202561e27
FrankLDF <frankluisduarte00@gmail.com>
2025-05-09 23:09:58 -0400

    Removing shadcn and tailwind settings for styles

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [react()],
     server: {
       port: env.VITE_PORT ? parseInt(env.VITE_PORT) : 5173,
     },
